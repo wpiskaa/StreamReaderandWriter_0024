a369dce89c85821e656d5ccc246c09936057ae16
wpiskaa <hafizzkurniawan19@gmail.com>
2025-06-09 10:43:57 +0700

    menambahkan penggunaan try-catch untuk akses indeks tidak valid

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,3 +4,18 @@
 #include <array>
 //untuk obyek array yang akan kita gunakan
 using namespace std;
+int main()
+{
+    cout << "Awal Program" << endl; //Penanda 1:...
+    try{
+        array<int, 3> data = { 1, 2, 3};
+        //Pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+}
