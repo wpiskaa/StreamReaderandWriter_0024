4921175f3c65120d16db078d859ae26fb022a909
wpiskaa <hafizzkurniawan19@gmail.com>
2025-06-09 10:31:40 +0700

    menambahkan demonstrasi pengecualian dengan tipe tidak cocok

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try{
+        cout << "Selamat Belajar Di Prodi TI UMY" << endl;
+        throw 0.5; "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+}
