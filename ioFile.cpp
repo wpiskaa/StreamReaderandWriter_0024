df27c257f8430086532cb5adf67bdd3127ee9ebd
wpiskaa <hafizzkurniawan19@gmail.com>
2025-06-09 10:04:52 +0700

    tambahkan loop tak terbatas untuk menulis ke file

--- ioFile.cpp
+++ ioFile.cpp
@@ -12,4 +12,17 @@ int main(){
     outfile.open("contohfile.txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+//unlimited loop untuk menulis
+while (true) {
+    cout << "- ";
+    //mendapatkan setiap karakter dalam satu baris
+    getline(cin, baris);
+    //loop akan berhenti jika anda memasukkan karakter q
+    if (baris == "q") break;
+    //menulis dan memasukkan nilai dari 'baris' ke dalam file
+    outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
 }
