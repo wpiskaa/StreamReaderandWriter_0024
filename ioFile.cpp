7a090a6aaaf127c069327ae135cf4cb1ece40f7c
wpiskaa <hafizzkurniawan19@gmail.com>
2025-06-09 10:19:25 +0700

    menutup file

--- ioFile.cpp
+++ ioFile.cpp
@@ -43,4 +43,7 @@ while (true) {
         //tutup file tersebut setelah selesai
         infile.close();
     }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
